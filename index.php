075bc3fd2f3c2818c3eb35b1a9790e16f93f7323
ZdenekZaruba <56403536+ZdenekZaruba@users.noreply.github.com>
2022-03-06 14:22:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="appData/style/css/style.min.css">
+    <title>Certifikace</title>
+</head>
+
+<body>
+    <?php
+    require_once "appData/scripts/php/function/_navBar.php";
+
+    require_once "appData/scripts/php/core/content.php";
+    ?>
+</body>
+
+</html>
